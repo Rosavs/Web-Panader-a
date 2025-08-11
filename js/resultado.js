d9c2c848bd3997244248fe0439926b100e4ab54c
Rosavs <133782846+Rosavs@users.noreply.github.com>
2025-08-12 01:41:47 +0200

    Add files via upload

---
+++ js/resultado.js
@@ -0,0 +1,15 @@
+
+	function confirmar(){
+		alert("Pedido Confirmado ¡Gracias!");
+		window.close("resultado.html");
+	}
+
+	function volver(){
+		alert("Vuelves a Pedidos");
+		window.close("resultado.html");
+	}
+
+	function inicio(){
+		alert("Vuelves a Inicio");
+		window.location.href = "index.html";
+	}
